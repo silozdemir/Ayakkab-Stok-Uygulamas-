625f0fc5344da965fa46d6bae83411ec9aac337c
silozdemir <silaozdemirr11@gmail.com>
2026-01-09 02:35:19 +0300

    Delete Ayakkabı Stok Uygulaması directory

--- Ayakkabı Stok Uygulaması/src/User.java
+++
@@ -1,8 +0,0 @@
-class User {
-    String kullaniciAdi, sifre, rol;
-    public User(String ad, String sifre, String rol) {
-        this.kullaniciAdi = ad;
-        this.sifre = sifre;
-        this.rol = rol;
-    }
-}
